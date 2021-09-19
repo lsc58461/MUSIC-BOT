97fdd2df92966eebe86c125e1cce217fae1dfe76
lsc58461 <79037820+lsc58461@users.noreply.github.com>
2021-09-20 05:13:45 +0900

    Update NIX_MUSIC.py

--- NIX_MUSIC.py
+++ NIX_MUSIC.py
@@ -4,11 +4,12 @@ import functools
 import itertools
 import math
 import random
-
 import discord
 import youtube_dl
+
 from async_timeout import timeout
 from discord.ext import commands, tasks
+from api import Rank, Normal, ARAM
 
 # Silence useless bug reports messages
 youtube_dl.utils.bug_reports_message = lambda: ''
@@ -585,6 +586,24 @@ async def on_ready():
     change_status.start()
     print('클라이언트로 로그인했습니다:\n{0.user.name}\n{0.user.id}'.format(bot))
 
+class MMR_Check(commands.Cog):
+
+    @commands.command(name='mmr')
+    async def _MMR(self, ctx: commands.Context, *, search: str):
+        async with ctx.typing():
+            _Rank = Rank(search)
+            _Normal = Normal(search)
+            _ARAM = ARAM(search)
+            embed = (discord.Embed(title='소환사 정보', description='```css\n{}\n```'.format(search), color=discord.Color.blurple())
+                .add_field(name='솔로랭크', value='```css\n{}\n```'.format(_Rank[0]), inline = False)
+                .add_field(name='노말', value='```css\n{}\n```'.format(_Normal[0]), inline = False)
+                .add_field(name='무작위 총력전', value='```css\n{}\n```'.format(_ARAM[0]), inline = False)
+                .set_author(name=ctx.author.name, icon_url=ctx.author.avatar_url))
+            await ctx.send(embed=embed)
+
+bot = commands.Bot(command_prefix='!', case_insensitive=True)
+bot.add_cog(MMR_Check(bot))
+    
 @bot.command(name='서버종료', aliases=['server_stop'])
 @commands.is_owner()
 async def botstop(ctx):
