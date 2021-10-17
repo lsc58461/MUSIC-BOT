e6091498d0316ffad28473b039b6eb443bff1caf
lsc58461 <79037820+lsc58461@users.noreply.github.com>
2021-10-17 22:57:58 +0900

    Update NIX_MUSIC.py

--- NIX_MUSIC.py
+++ NIX_MUSIC.py
@@ -41,7 +41,7 @@ class YTDLSource(discord.PCMVolumeTransformer):
 
     FFMPEG_OPTIONS = {
         'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5',
-        'options': '-vn -6',
+        'options': '-vn',
     }
 
     ytdl = youtube_dl.YoutubeDL(YTDL_OPTIONS)
